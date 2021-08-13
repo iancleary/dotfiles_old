1b813a8f513d3d2900b2e20bac91a78c666286c1
iancleary <hello@iancleary.me>
2021-08-12 21:07:53 -0700

    delete scripts folder

--- scripts/main_rename.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-curl --silent --location --output - https://raw.githubusercontent.com/PurpleBooth/change-github-default-branch.sh/main/change-github-default-branch.sh | \
-    bash -s -- \
-    -t ca843b9a9b8904b5bd9a2f17309a85bdd6282d8a -d main iancleary/spa-fastrf
