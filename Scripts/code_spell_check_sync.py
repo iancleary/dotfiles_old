2c1b236c2248e6a3b1ea6f3d42e276cb46304158
icancclearynow <hello@iancleary.me>
2021-11-27 11:45:17 -0700

    Linting code_spell_check_sync.py

--- Scripts/code_spell_check_sync.py
+++ Scripts/code_spell_check_sync.py
@@ -26,11 +26,6 @@ def merge_lists(cspell_word_list_one:List[str], cspell_word_list_two:List[str]) 
     union = list_one_set.union(list_two_set)
     return sorted(list(union))
 
-macos_code_settings_cspell_words = get_cspell_words(MACOS_CODE_SETTINGS_FILE)
-linux_code_settings_cspell_words = get_cspell_words(LINUX_CODE_SETTINGS_FILE)
-
-combined_cspell_words = merge_lists(macos_code_settings_cspell_words, linux_code_settings_cspell_words)
-
 def update_cspell_words(settings_file:Path, cspell_words:List[str]) -> None:
     with open(settings_file, "r") as file:
         settings = json.load(file)
@@ -40,6 +35,10 @@ def update_cspell_words(settings_file:Path, cspell_words:List[str]) -> None:
         json_object = json.dumps(settings, indent = 4)
         file.write(json_object)
 
+macos_code_settings_cspell_words = get_cspell_words(MACOS_CODE_SETTINGS_FILE)
+linux_code_settings_cspell_words = get_cspell_words(LINUX_CODE_SETTINGS_FILE)
+
+combined_cspell_words = merge_lists(macos_code_settings_cspell_words, linux_code_settings_cspell_words)
 
 update_cspell_words(settings_file=MACOS_CODE_SETTINGS_FILE, cspell_words=combined_cspell_words)
 update_cspell_words(settings_file=LINUX_CODE_SETTINGS_FILE, cspell_words=combined_cspell_words)
